eb30227a889d2f3e78c3940a06c31ed239963b1f
Peter Bräuer <pb866.git@gmail.com>
2020-04-06 11:20:52 +0200

    WIP: Warn of misplaced file; restructure routines
    New function setup_download to connect to the ICARE server, scan for
    available files, check local directory for already downloaded files and
    save lists of files (folder path + file name) with structure on ICARE
    server, intended structure on local server and any misplaced files in
    already existing local folders. New warning log with entries for missing
    data folders on ICARE server and misplaced files in local directories.
    Return to use only one connection for ICARE and not a new connection for
    every date subfolder to avoid connection problems because of too many
    open ftp connections.
    New function rm_misplacedfiles which deletes misplaced files in local
    data folders. Options exist to delete selected, remaining, all or no
    files.

--- src/ICARE.jl
+++ src/ICARE.jl
@@ -112,4 +112,145 @@ function ftp_download(
   return localfiles
 end #function download
 
+
+"""
+    setup_download(
+      user::String,
+      password::String,
+      product::String,
+      startdate::Date,
+      enddate::Date,
+      version::Float64 = 4.20;
+      dir::String = ".",
+      warnlog::String = "ICAREwarnings.log"
+    ) -> remotefiles, localfiles, misplacedfiles
+
+Connect to ICARE server with `user` login name and `password` and scan for available
+files in the timeframe `startdate` to `enddate` for the `version` of the given `product`.
+Sync the folder structure to the local `dir`ectory and warn of missing data folders
+on the ICARE server or misplaced files in already existing local folders in the
+`warnlog` file.
+
+Returns a `Vector{String}` with absolute folder path and file names for remote
+and local directory as well as for misplaced files in the local directories.
+"""
+function setup_download(
+  user::String,
+  password::String,
+  product::String,
+  startdate::Date,
+  enddate::Date,
+  version::Float64 = 4.20;
+  dir::String = ".",
+  warnlog::String = "ICAREwarnings.log"
+)
+  # Start file logger
+  logio = open(warnlog, "w+")
+  logger = logg.SimpleLogger(logio, logg.Debug)
+  # Connect to ICARE server
+  ftp.ftp_init()
+  icare = ftp.FTP(hostname = "ftp.icare.univ-lille1.fr",
+    username = user, password = password)
+  # Define main folder for selected data type
+  caliopdir = "/SPACEBORNE/CALIOP/"
+  datadir = @sprintf "%s.v%.2f" product version
+  # Define dates in range
+  years = [string(y) for y = Dates.year(startdate):Dates.year(enddate)]
+  dates = [Dates.format(d, "yyyy_mm_dd") for d = startdate:Dates.Day(1):enddate]
+  ## Sync data files
+  remotefiles = String[]; localfiles = String[]; misplacedfiles = String[]
+  prog = pm.Progress(length(dates), "setup...")
+  for year in years, date in dates
+    # Only look for dates with matching years
+    startswith(date, year) || continue
+    # Define current remote directory from date
+    remotedir = joinpath(caliopdir, datadir, year, date)
+    localdir = joinpath(dir, datadir, year, date)
+    try
+      # Get all data files in date range
+      cd(icare, remotedir)
+      remfiles = readdir(icare)
+      # Sync local folder structure with ftp server
+      if isdir(localdir)
+        # Find additional (misplaced) files in local directory
+        push!(misplacedfiles, joinpath.(localdir,
+          setdiff(readdir(localdir)[.!startswith.(readdir(localdir), ".")],
+          remfiles))...)
+        # Remember missing files in local directory for download
+        for file in remfiles
+          if !isfile(joinpath(localdir, file))
+            push!(remotefiles, joinpath(remotedir, file))
+            push!(localfiles, joinpath(localdir, file))
+          end
+        end
+      else
+        # Create missing folders from remote directory in local directory
+        mkpath(localdir)
+        push!(remotefiles, joinpath.(remotedir, remfiles)...)
+        push!(localfiles, joinpath.(localdir, remfiles)...)
+      end
+    catch err
+      if isdir(remotedir)
+        rethrow(err)
+      else
+        logg.with_logger(logger) do
+          println(logio, "no data for $date")
+        end
+        flush(logio)
+        isdir(localdir) && push!(misplacedfiles, localdir)
+      end
+    end
+    # Monitor progress for progress bar
+    pm.next!(prog, showvalues = [(:date,date)])
+  end #loop over dates
+  # Clean-up
+  pm.finish!(prog)
+  # Close current FTP connection to ICARE server
+  close(icare)
+  ftp.ftp_cleanup()
+  # Warn of misplaced files in log file
+  length(misplacedfiles) > 0 && logg.with_logger(logger) do
+    println(logio, "\nThe following misplaced files have been detected in local data folders:")
+    foreach(x -> println(logio, x), misplacedfiles)
+  end
+  close(logio)
+
+  return remotefiles, localfiles, misplacedfiles
+end #function setup_download
+
+
+"""
+    rm_misplacedfiles(misplacedfiles::Vector{String})
+
+Delete `misplacedfiles`. The routine loops over `misplacedfiles` and deletes them
+with the following confirmation options:
+
+- `y`: current file is deleted
+- `n`: current file deletion is skipped
+- `all`: press at any time; __all files__ are deleted __even previously skipped files__
+- `remaining`: the current and __all remaining files__ in the list are deleted;
+  _previously skipped files are kept_
+- `none`: __all remaining files__ are kept (previously deleted files are __not__ restored)
+"""
+function rm_misplacedfiles(misplacedfiles::Vector{String})
+  length(misplacedfiles) > 0 && @warn "misplaced files in local data folders detected"
+  for file in misplacedfiles
+    println("Delete $file?")
+    print("(y/n/remaining/all): ")
+    confirm = readline()
+    if startswith(lowercase(confirm), "y")
+      rm(file, recursive=true)
+    elseif lowercase(confirm) == "all"
+      rm.(misplacedfiles, recursive=true)
+      break
+    elseif lowercase(confirm) == "remaining"
+      i = findfirst(misplacedfiles.==file)
+      rm.(misplacedfiles[i:end], recursive=true)
+      break
+    elseif lowercase(confirm) == "none"
+      break
+    end
+  end
+end
+
 end #module ICARE
