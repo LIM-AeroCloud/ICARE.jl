78e30e8ecd5711b89b71dc0c6816c3fac776ce77
Peter Bräuer <pb866.git@gmail.com>
2025-09-01 22:45:14 +0200

    Initial download test
    Signed-off-by: Peter Bräuer <pb866.git@gmail.com>

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,11 +1,23 @@
-using Test
+using Test, ICARE
 
-# user = get(ENV, "ICARE_USER", (print("ICARE user name: "), readline()))
-# password = get(ENV, "ICARE_PASSWORD", (print("ICARE password: "), readline()))
+user = get(ENV, "ICARE_USER", nothing)
+password = get(ENV, "ICARE_PASSWORD", nothing)
+
+if isnothing(user) || isnothing(password)
+    @error "Either ICARE_USER or ICARE_PASSWORD not set as environment variable; test aborted"
+    exit(1)
+else
+    user = ICARE.SFTP.URIs.unescapeuri(user)
+    password = ICARE.SFTP.URIs.unescapeuri(password)
+end
 
 @testset "ICARE_USER secret test" begin
-    icare_user = get(ENV, "ICARE_USER", nothing)
-    @test !isnothing(icare_user)  # Check that ICARE_USER is set
-    # You can add further tests using icare_user
-    # e.g. @test icare_user == "expected_value"
+    mktempdir() do root
+        @show root
+        cp("data/05kmCPro.v4.51/", joinpath(root, "05kmCPro.v4.51/"), force=true)
+        ICARE.sftp_download(user, password, "05kmCPro", 20060612, localroot = root, convert=false)
+        ICARE.sftp_download(user, password, "05kmCPro", 20060612, localroot = root)
+        @test all(isfile.(joinpath.(root, "05kmCPro.v4.51", "2006", "2006_06_12",
+            "CAL_LID_L2_05kmCPro-Standard-V4-51.2006-06-12T00-53-43ZN".*[".hdf", ".h5"])))
+    end
 end
